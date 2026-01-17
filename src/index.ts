fc55569b53f04d38efede4c2a74386b911d36997
Dom Christie <christiedom@gmail.com>
2026-01-17 20:41:55 +0000

    Rewrite with navigation API

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-type Direction = 'forward' | 'back'
+type Direction = 'forward' | 'back' | 'none'
 type NavigationTypeString = 'push' | 'replace' | 'traverse'
 type Config = {
 	from: URL,
@@ -7,7 +7,6 @@ type Config = {
 	srcElement?: Element,
 	trigger?: Event,
 	navigationType: NavigationTypeString,
-	historyState?: State,
 
 	method?: 'GET' | 'POST' | 'PUT' | 'DELETE',
 	body?: string | FormData | URLSearchParams,
@@ -18,30 +17,14 @@ type Config = {
 	mediaType?: string,
 	text?: string,
 }
-type State = {
-	index: number
-	scrollX: number
-	scrollY: number
-}
 
 let DIRECTION_ATTR = 'data-hop-direction'
 let PERSIST_ATTR = 'data-hop-persist'
 let DISABLED_ATTR = 'data-hop'
 
 let started = false
-
-let abortController: AbortController | undefined
 let currentTransition: ViewTransition | undefined
-
-// When we traverse the history, the window.location is already set to the new location.
-// This variable tells us where we came from
-let currentUrl: URL = new URL(location.href)
-
-// The History API does not tell you if navigation is forward or back, so
-// you can figure it using an index. On pushState the index is incremented so you
-// can use that to determine popstate if going forward or back.
-let currentHistoryIndex = 0
-let parser = new DOMParser()
+let parser
 
 function enabled(el: Element | Document = document) {
 	if (el instanceof Document) {
@@ -51,54 +34,18 @@ function enabled(el: Element | Document = document) {
 	}
 }
 
-let isSamePage = (url: URL, otherUrl: URL) => url.pathname === otherUrl.pathname && url.search === otherUrl.search
-let isRefresh = (from: URL, to: URL, navigationType: NavigationType) => from.pathname === to.pathname && navigationType === 'replace'
-let refreshScrollBehavior = () => document.querySelector('meta[name="hop-refresh-scroll"]')?.getAttribute('content')
-
-let send = (el: Element | Document = document, type: string, detail?: any, bub?: boolean) => el.dispatchEvent(new CustomEvent("hop:" + type, { detail, cancelable: true, bubbles: bub !== false, composed: true }))
-
-let leavesWindow = (ev: MouseEvent) =>
-	(ev.button && ev.button !== 0) || // left clicks only
-	ev.metaKey || // new tab (mac)
-	ev.ctrlKey || // new tab (windows)
-	ev.altKey || // download
-	ev.shiftKey // new window
-
-// form.action and form.method can point to an <input name="action"> or <input name="method">
-// in which case should fallback to the form attribute
-let formAttr = (form: HTMLFormElement, submitter: HTMLElement | null, attr: string, defaultVal: any) =>
-	submitter?.getAttribute(`form${attr}`) ?? (form[attr] === 'string' ? form[attr] : form.getAttribute(attr)) ?? defaultVal
-
-// only update history entries that are managed by us
-// leave other entries alone and do not accidentally add state.
-function saveScrollPosition() {
-	if (history.state) {
-		history.scrollRestoration = 'manual'
-		// avoid expensive calls to history.replaceState
-		if (scrollX !== history.state.scrollX || scrollY !== history.state.scrollY) {
-			history.replaceState({ ...history.state, scrollX, scrollY }, '')
-		}
+let isHashChange = (ev) => (ev.hashChange && !ev.sourceElement) || (ev.hashChange && ev.sourceElement.matches('a[href^="#"]'))
+function direction(ev) {
+	if (ev.navigationType === 'traverse') {
+		return ev.destination.index > navigation.currentEntry.index ? 'forward' : 'back'
+	} else if (['replace', 'reload'].includes(ev.navigationType)) {
+		return 'none'
+	} else {
+		return 'forward'
 	}
 }
 
-function announce() {
-	let div = document.createElement('div')
-	div.setAttribute('aria-live', 'assertive')
-	div.setAttribute('aria-atomic', 'true')
-	Object.assign(div.style, { position: 'absolute', left: '0', top: '0', clip: 'rect(0 0 0 0)', clipPath: 'inset(50%)', overflow: 'hidden', whiteSpace: 'nowrap', width: '1px', height: '1px' })
-
-	document.body.append(div)
-	setTimeout(
-		() => {
-			let title = document.title || document.querySelector('h1')?.textContent || location.pathname
-			div.textContent = title
-		},
-		// Much thought went into this magic number; the gist is that screen readers
-		// need to see that the element changed and might not do so if it happens
-		// too quickly.
-		60
-	)
-}
+let send = (el: Element | Document = document, type: string, detail?: any, bub?: boolean) => el.dispatchEvent(new CustomEvent("hop:" + type, { detail, cancelable: true, bubbles: bub !== false, composed: true }))
 
 async function fetchHtml(cfg: Config): Promise<Document | undefined> {
 	try {
@@ -124,6 +71,7 @@ async function fetchHtml(cfg: Config): Promise<Document | undefined> {
 		cfg.to = redirectedTo
 	}
 
+	parser = parser || new DOMParser()
 	let newDoc = parser.parseFromString(cfg.text, cfg.mediaType)
 	newDoc.querySelectorAll('noscript').forEach((el) => el.remove())
 
@@ -135,6 +83,24 @@ async function fetchHtml(cfg: Config): Promise<Document | undefined> {
 	links.length && !cfg.signal.aborted && (await Promise.all(links))
 
 	return newDoc
+}
+
+function preloadStyles(newDoc: Document) {
+	let oldEls = [...document.querySelectorAll('head link[rel=stylesheet]')]
+	let newEls = [...newDoc.querySelectorAll('head link[rel=stylesheet]')]
+
+	return newEls
+		.filter(newEl => !oldEls.some(oldEl => oldEl.isEqualNode(newEl))) // todo: consider persistent stylesheets
+		.map((el) => {
+			let link = document.createElement('link')
+			link.setAttribute('rel', 'preload')
+			link.setAttribute('as', 'style')
+			link.setAttribute('href', el.getAttribute('href')!)
+			return new Promise((resolve) => {
+				['load', 'error'].forEach((ev) => link.addEventListener(ev, resolve))
+				document.head.append(link)
+			})
+		})
 }
 
 function swapRootAttributes(newDoc: Document) {
@@ -210,6 +176,15 @@ function flagNewScripts(scripts: HTMLCollectionOf<HTMLScriptElement>) {
 	for (let script of scripts) (script as any).__new = true
 }
 
+function scroll(from, to, ev) {
+	let isRefresh = from.pathname === to.pathname && ev.navigationType === 'replace'
+	let preserveScroll = isRefresh && document.querySelector('meta[name="hop-refresh-scroll"]')?.getAttribute('content') === 'preserve'
+	if (!preserveScroll) {
+		window.scrollTo(0, 0)
+		ev.scroll()
+	}
+}
+
 function runScripts() {
 	let runnable = [...document.scripts].filter(
 		script => (script as any).__new && script.dataset.hopEval !== 'false'
@@ -248,61 +223,41 @@ function runScripts() {
 	return wait
 }
 
-function preloadStyles(newDoc: Document) {
-	let oldEls = [...document.querySelectorAll('head link[rel=stylesheet]')]
-	let newEls = [...newDoc.querySelectorAll('head link[rel=stylesheet]')]
+function announce() {
+	let div = document.createElement('div')
+	div.setAttribute('aria-live', 'assertive')
+	div.setAttribute('aria-atomic', 'true')
+	Object.assign(div.style, { position: 'absolute', left: '0', top: '0', clip: 'rect(0 0 0 0)', clipPath: 'inset(50%)', overflow: 'hidden', whiteSpace: 'nowrap', width: '1px', height: '1px' })
 
-	return newEls
-		.filter(newEl => !oldEls.some(oldEl => oldEl.isEqualNode(newEl))) // todo: consider persistent stylesheets
-		.map((el) => {
-			let link = document.createElement('link')
-			link.setAttribute('rel', 'preload')
-			link.setAttribute('as', 'style')
-			link.setAttribute('href', el.getAttribute('href')!)
-			return new Promise((resolve) => {
-				['load', 'error'].forEach((ev) => link.addEventListener(ev, resolve))
-				document.head.append(link)
-			})
-		})
+	document.body.append(div)
+	setTimeout(
+		() => {
+			let title = document.title || document.querySelector('h1')?.textContent || location.pathname
+			div.textContent = title
+		},
+		// Much thought went into this magic number; the gist is that screen readers
+		// need to see that the element changed and might not do so if it happens
+		// too quickly.
+		60
+	)
 }
 
-export async function hop(to: URL | string, options: Partial<Config>) {
-	to = to instanceof URL ? to : new URL(to, location.href)
-	abortController?.abort()
-	abortController = new AbortController()
-
-	// Check eligibility
-	if (!enabled() || location.origin !== to.origin) {
-		location.href = to.href
-		return
-	}
+export async function hop(ev) {
+	let from = new URL(location.href)
+	let to = new URL(ev.destination.url)
 
 	let cfg: Config = {
-		from: currentUrl,
+		from,
 		to,
-		direction: 'forward',
-		navigationType: options.historyState ? 'traverse' : (options.navigationType === 'replace' ? 'replace' : 'push'),
-		signal: abortController.signal,
-		...options
+		direction: direction(ev),
+		navigationType: ev.navigationType,
+		signal: ev.signal
 	}
+	if (!send(cfg.srcElement, 'config', { cfg })) return
 
-	if (cfg.navigationType !== 'traverse') saveScrollPosition()
-	if (isSamePage(cfg.from, cfg.to) && !cfg.body) {
-		if ((cfg.direction === 'forward' && cfg.to.hash) || (cfg.direction === 'back' && cfg.from.hash)) {
-			moveToLocation(document.title)
-			return
-		}
-	}
-
-	let newDoc: Document | undefined
-	if (send(cfg.srcElement, 'config', { cfg })) {
-		if (newDoc = await fetchHtml(cfg)) {
-			if (cfg.navigationType !== 'traverse') saveScrollPosition()
-		} else {
-			location.href = cfg.to.href
-			return
-		}
-	} else {
+	let newDoc: Document | undefined = await fetchHtml(cfg)
+	if (!newDoc) {
+		location.href = cfg.to.href
 		return
 	}
 
@@ -315,218 +270,58 @@ export async function hop(to: URL | string, options: Partial<Config>) {
 
 	document.documentElement.setAttribute(DIRECTION_ATTR, cfg.direction)
 
-	let domUpdated: Promise<void> = Promise.resolve()
-	let transitionFinished: Promise<void> = Promise.resolve()
+	let swapped: Promise<void> = Promise.resolve()
+	let transitioned: Promise<void> = Promise.resolve()
 	if (document.startViewTransition) {
-		// This automatically cancels any previous transition
-		// We also already took care that the earlier update callback got through
-		currentTransition = document.startViewTransition(
-			async () => await updateDOM(newDoc)
-		)
-		domUpdated = currentTransition.updateCallbackDone
-		transitionFinished = currentTransition.finished
+		currentTransition = document.startViewTransition(swap)
+		swapped = currentTransition.updateCallbackDone
+		transitioned = currentTransition.finished
 	} else {
-		await updateDOM(newDoc)
+		swap()
 	}
-	domUpdated.finally(async () => {
+
+	swapped.finally(async () => {
 		send(document, 'swapped', { cfg })
+		scroll(from, to, ev)
 		await runScripts()
 		send(document, 'load')
 		announce()
 	})
-	transitionFinished.finally(() => {
+
+	return transitioned.finally(() => {
 		currentTransition = void 0
 		document.documentElement.removeAttribute(DIRECTION_ATTR)
 	})
 
-	async function updateDOM(newDoc: Document) {
-		const title = document.title
+	function swap() {
+		if (!newDoc) return
 		swapRootAttributes(newDoc)
 		swapHeadElements(newDoc)
 		withRestoredFocus(() => {
 			swapBodyElement(newDoc.body)
 		})
-		moveToLocation(title)
-	}
-
-	function moveToLocation(title: string) {
-		updateHistory(title)
-		scroll()
-	}
-
-	function updateHistory(title: string) {
-		const { to, navigationType } = cfg
-
-		const targetTitle = document.title
-		document.title = title
-
-		if (to.href !== location.href && !cfg.historyState) {
-			if (navigationType === 'replace') {
-				history.replaceState(history.state, '', to.href)
-			} else {
-				history.pushState({ index: ++currentHistoryIndex, scrollX: 0, scrollY: 0 }, '', to.href)
-			}
-		}
-		document.title = targetTitle
-		currentUrl = to
-	}
-
-	function scroll() {
-		let { from, to, historyState, navigationType } = cfg
-		let scrollToOpts: ScrollToOptions = { left: 0, top: 0, behavior: 'instant' }
-
-		if (historyState) {
-			scrollToOpts = { left: historyState.scrollX, top: historyState.scrollY }
-		} else if (isRefresh(to, from, navigationType) && refreshScrollBehavior() === 'preserve') {
-			history.scrollRestoration = 'manual'
-			return
-		} else {
-			if (to.hash) {
-				// because we are already on the target page ...
-				// ... what comes next is a intra-page navigation
-				// that won't reload the page but instead scroll to the fragment
-				history.scrollRestoration = 'auto'
-				const savedState = history.state
-				location.href = to.href; // this kills the history state on Firefox
-				if (!history.state) {
-					history.replaceState(savedState, '') // this restores the history state
-					if (isSamePage(from, to)) window.dispatchEvent(new PopStateEvent('popstate'))
-				}
-				history.scrollRestoration = 'manual'
-				return
-			}
-		}
-		scrollTo(scrollToOpts)
-		history.scrollRestoration = 'manual'
 	}
 }
 
 // initialization
 function start() {
-	if (started || !enabled()) return
+	if (started || !enabled() || !('navigation' in window)) return
 
-	let lastClickedElementLeavingWindow: EventTarget | null = null
+	navigation.addEventListener('navigate', function (ev) {
+		let srcElement = ev.info?.srcElement || ev.sourceElement || undefined
+		if (!ev.canIntercept || ev.downloadRequest || isHashChange(ev) || !enabled(srcElement)) return
 
-	addEventListener('click', (ev) => {
-		let link = ev.target
-		lastClickedElementLeavingWindow = leavesWindow(ev) ? link : null
-
-		// Check eligibility
-		if (ev.composed) link = ev.composedPath()[0]
-		if (link instanceof Element) link = link.closest('a, area')
-		if (
-			!(link instanceof HTMLAnchorElement) &&
-			!(link instanceof SVGAElement) &&
-			!(link instanceof HTMLAreaElement)
-		) return
-		const linkTarget = link instanceof SVGAElement ? link.target.baseVal : link.target
-		const href = link instanceof SVGAElement ? link.href.baseVal : link.href
-		if (
-			!enabled(link) ||
-			link.hasAttribute('download') ||
-			!link.href ||
-			(linkTarget && linkTarget !== '_self') ||
-			new URL(href, location.href).origin !== location.origin ||
-			lastClickedElementLeavingWindow ||
-			ev.defaultPrevented
-		) return
-
-		ev.preventDefault()
-		hop(href, {
-			srcElement: link,
-			navigationType: link.dataset.hopType === 'replace' ? 'replace' : undefined
+		ev.intercept({
+			async handler() {
+				if (ev.navigationType !== 'replace' && srcElement?.closest('[data-hop-type="replace"]')) {
+					return navigation.navigate(ev.destination.url, { history: 'replace', info: {srcElement} }).finished
+				} else {
+					await hop(ev)
+				}
+			},
+			scroll: 'manual'
 		})
 	})
-
-	addEventListener('submit', (ev) => {
-		let el = ev.target as HTMLElement
-		let submitter = ev.submitter
-
-		let clickedWithKeys = submitter && submitter === lastClickedElementLeavingWindow
-		lastClickedElementLeavingWindow = null
-
-		// Check eligibility
-		if (el.tagName !== 'FORM' || ev.defaultPrevented || !enabled(el) || clickedWithKeys) return
-		let form = el as HTMLFormElement
-		let action = new URL(formAttr(form, submitter, 'action', location.pathname), location.href)
-		let method = formAttr(form, submitter, 'method', 'get')
-		let enctype = formAttr(form, submitter, 'enctype', 'application/x-www-form-urlencoded')
-		if (method === 'dialog' || location.origin !== new URL(action, location.href).origin) return
-
-		let body: FormData | URLSearchParams | undefined = new FormData(form, submitter)
-		if (method === 'get') {
-			let params = new URLSearchParams(body as any)
-			action.search = params.toString()
-			body = void 0
-		} else if (enctype === 'application/x-www-form-urlencoded') {
-			body = new URLSearchParams(body as any)
-		}
-		ev.preventDefault()
-		hop(action, {
-			srcElement: submitter ?? form,
-			method,
-			body,
-			navigationType: (submitter ?? form).dataset.hopType === 'replace' ? 'replace' : undefined
-		})
-	})
-
-	addEventListener('popstate', function(ev: PopStateEvent) {
-		if (!enabled() && ev.state) {
-			// The current page doesn't have transitions enabled
-			// but the page we navigate to does (because it set the state).
-			// Do a full page refresh to reload the client-side router from the new page.
-			return location.reload()
-		}
-
-		// Our transition entries always have state. Ignore stateless entries.
-		if (ev.state === null) return
-		const state: State = history.state
-		const nextIndex = state.index
-		const direction: Direction = nextIndex > currentHistoryIndex ? 'forward' : 'back'
-		currentHistoryIndex = nextIndex
-		hop(location.href, { direction, from: currentUrl, historyState: state })
-	})
-
-	if (history.state) {
-		// Here we reloaded a page with history state
-		// (e.g. history navigation from non-transition page or browser reload)
-		currentHistoryIndex = history.state.index
-		scrollTo({ left: history.state.scrollX, top: history.state.scrollY })
-	} else if (enabled()) {
-		// This page is loaded from the browser address bar or via a link from extern,
-		// it needs a state in the history
-		history.replaceState({ index: currentHistoryIndex, scrollX, scrollY }, '')
-		history.scrollRestoration = 'manual'
-	}
-
-	// There's not a good way to record scroll position before a history back
-	// navigation, so we will record it when the user has stopped scrolling.
-	if ('onscrollend' in window) addEventListener('scrollend', saveScrollPosition)
-	else {
-		let intervalId: number | undefined, lastY: number, lastX: number, lastIndex: State['index']
-		function scrollInterval() {
-			// Check the index to see if a popstate event was fired
-			if (lastIndex !== history.state?.index) return reset()
-
-			// Check if the user stopped scrolling
-			if (lastY === scrollY && lastX === scrollX) {
-				saveScrollPosition()
-				return reset()
-			} else {
-				(lastY = scrollY), (lastX = scrollX)
-			}
-		}
-		function reset() {
-			clearInterval(intervalId)
-			intervalId = void 0
-		}
-		// We can't know when or how often scroll events fire, so we'll just use them to start intervals
-		addEventListener('scroll', function () {
-			if (intervalId !== undefined) return
-			(lastIndex = history.state?.index), (lastY = scrollY), (lastX = scrollX)
-			intervalId = window.setInterval(scrollInterval, 50)
-		})
-	}
 	started = true
 }
 addEventListener('DOMContentLoaded', start)
