1e0b8f69c280a790324f4abadea7c67309b43c71
Dom Christie <christiedom@gmail.com>
2026-01-31 14:06:55 +0000

    Improve scroll fix

--- grasshopper.js
+++ grasshopper.js
@@ -252,7 +252,8 @@ function attachShadowRoots(root) {
 
 async function scroll(navEvent) {
 	await sendInterceptable(document, 'before-scroll')
-	scrollTo(0, 0) // Fix when navigating from a scrolled page in Chrome/Safari
+	if (['push', 'replace'].includes(navEvent.navigationType))
+		scrollTo(0, 0) // Fix when navigating from a scrolled page in Chrome/Safari
 	navEvent.scroll()
 	send(document, 'scrolled')
 }
